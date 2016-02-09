591a08ea8d7146aa5a60e88a4699a62e86b71c5d
Jane <jane.e.reynolds@gmail.com>
2016-02-09 15:20:31 -0800

    Add custom jQuery to push footer beneath content on all pages (i.e. dynamically sized sticky footer)

---
+++ js/custom-js.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+	$('.main').css('padding-bottom', $('.footer').height());
+}); 
+
+// Move footer down when page resizes
+var bumpIt = function() {  
+  	$('.main').css('padding-bottom', $('.footer').height());
+	},
+	didResize = false;
+
+bumpIt();
+
+$(window).resize(function() {
+	didResize = true;
+});
+
+setInterval(function() {
+	if(didResize){
+		didResize = false;
+		bumpIt();
+	}
+}, 250);
