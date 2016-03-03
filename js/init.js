37e3a18daf4a1700498540af232ca5005ff02c51
Jane <jane.e.reynolds@gmail.com>
2016-03-03 11:18:49 -0800

    Find and clean up call to Countdown Timer in init.js

--- js/init.js
+++ js/init.js
@@ -402,28 +402,21 @@ availiableExamples = {
 	set5minFromNow  : 5 * 60 * 1000,
 	set1minFromNow  : 1 * 60 * 1000
 };
+
 function callback(event) {
 	var $this = $(this);
-	switch(event.type) {
-		case "seconds":
-		case "minutes":
-		case "hours":
-		case "days":
-		case "weeks":
-		case "daysLeft":
-			$this.find('div span#'+event.type).html(event.value);
-			if(finished) {
+	if (event.type == 'finished') {
+		$this.fadeTo('slow', .5);
+		finished = true;
+	} else {  // i.e. "seconds", "minutes", "hours" etc
+		$this.find('div span#'+event.type).html(event.value);
+		if(finished) {
 				$this.fadeTo(0, 1);
 				finished = false;
-			}
-
-			break;
-		case "finished":
-			$this.fadeTo('slow', .5);
-			finished = true;
-			break;
+		}
 	}
 }
+
 $('#counter').countdown(target.valueOf(), callback);
 
 // Pages Design Functions
